eed949f07e684eb74de71fa4952d62f2b39150ef
TaylorBisset <87632154+TaylorBisset@users.noreply.github.com>
2023-05-11 12:58:27 -0700

    Inner if statement to verify day of the week

--- SleepIn/SleepIn.cpp
+++ SleepIn/SleepIn.cpp
@@ -9,17 +9,25 @@ int main()
 
 	cout << "Are you on vacation? (y/n) : ";
 	cin >> onVacation;
-	if (onVacation == 'n') 
-	{
-		cout << "What day of the week is it?\n";
-		cout << "1 = Sunday\n2 = Monday\n3 = Tuesday\n4 = Wednesday\n5 = Thursday\n6 = Friday\n7 = Saturday\n";
-		cout << "\nDay? ";
+		if (onVacation == 'n')
+		{
+			cout << "\nWhat day of the week is it?\n";
+			cout << "1 = Sunday\n2 = Monday\n3 = Tuesday\n4 = Wednesday\n5 = Thursday\n6 = Friday\n7 = Saturday\n";
+			cout << "\nDay? ";
 
-		
-	}
-	else
-	{
-		cout << "\nYou can sleep in!\n";
-	}
-	
+			cin >> dayOfWeek;
+			if (dayOfWeek > 1 && dayOfWeek < 7)
+			{
+				cout << "\nGot to work, sleepy-head!\n";
+			}
+			else
+			{
+				cout << "\nYou can sleep in!\n";
+			}
+		}
+		else if (onVacation == 'y')
+		{
+			cout << "\nYou can sleep in!\n";
+		}
+
 }
