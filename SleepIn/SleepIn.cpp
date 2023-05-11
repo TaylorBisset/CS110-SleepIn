545edf21decb0c8d703572c180f36c70ab0f3211
TaylorBisset <87632154+TaylorBisset@users.noreply.github.com>
2023-05-11 12:45:55 -0700

    Create initial user prompt

--- SleepIn/SleepIn.cpp
+++ SleepIn/SleepIn.cpp
@@ -4,5 +4,22 @@ using namespace std;
 
 int main()
 {
+	char onVacation = 'n';
+	int dayOfWeek = 1;
 
+	cout << "Are you on vacation? (y/n) : ";
+	cin >> onVacation;
+	if (onVacation == 'n') 
+	{
+		cout << "What day of the week is it?\n";
+		cout << "1 = Sunday\n2 = Monday\n3 = Tuesday\n4 = Wednesday\n5 = Thursday\n6 = Friday\n7 = Saturday\n";
+		cout << "\nDay? ";
+
+		
+	}
+	else
+	{
+		cout << "\nYou can sleep in!\n";
+	}
+	
 }
